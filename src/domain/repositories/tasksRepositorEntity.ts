4da857523db3bda8f5739c907d3ff35a2e89f2f8
pedrobzz <pedrobzz66@gmail.com>
2022-07-08 20:32:05 -0300

    feat(Entity - TasksRepository): Added Tasks Repository

---
+++ src/domain/repositories/tasksRepositorEntity.ts
@@ -0,0 +1,14 @@
+import { Task } from "../entities";
+
+export interface TasksRepositorEntity {
+  getAllTasks: () => Promise<Task[]>;
+  getTaskById: (id: string) => Promise<Task>;
+  createTask: (
+    task: Omit<Task, "id" | "createdAt" | "updatedAt">,
+  ) => Promise<Task>;
+  updateTask: (
+    id: string,
+    task: Partial<Omit<Task, "id" | "createdAt" | "updatedAt">>,
+  ) => Promise<Task>;
+  deleteTask: (id: string) => Promise<void>;
+}
