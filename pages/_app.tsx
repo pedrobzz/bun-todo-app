f7494e324b2760d1d9cb21f7b22a335bb4127609
pedrobzz <pedrobzz66@gmail.com>
2022-07-09 00:57:24 -0300

    feat(TasksContext): Added TasksContextProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { TasksContextProvider } from "applications/context/tasksContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <TasksContextProvider>
+      <Component {...pageProps} />
+    </TasksContextProvider>
+  );
 }
 
 export default MyApp;
